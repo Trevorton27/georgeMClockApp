7f3efbd38f46c7e43a5c0922931f2696feb83817
Trevor Mearns <spiral272@gmail.com>
2021-08-03 12:04:24 +0900

    ready for code review approval

--- script.js
+++ script.js
@@ -1,69 +1,84 @@
-function digitalClock() {
+function renderClock() {
+  const timeObject = new Date();
+  let hours = timeObject.getHours();
+  const minutes = renderLeadingZero(timeObject.getMinutes());
+  const seconds = renderLeadingZero(timeObject.getSeconds());
+  const isAm = hours < 12 || hours === 0;
+  let amPm = isAm ? 'AM' : 'PM';
 
-    const clockDisplay = document.getElementById("timeDisplay");
-    const today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
-    clockDisplay.textContent = `${hours}:${minutes}:${seconds}` + " hrs"
-
-
-    const dateDisplay = document.getElementById("dateDisplay");
-    const dateToday = new Date();
-    let day = dateToday.getDay();
-    let month = dateToday.getMonth();
-    let date = dateToday.getDate();
-    let year = dateToday.getFullYear();
-
-    if (date === 1 || date === 21 || date === 31) {
-        date = date + "st";
-    }
-    else if (date === 2 || date === 22) {
-        date = date + "nd";
-    }
-    else if (date === 3 || date === 23) {
-        date = date + "rd";
-    }
-    else {
-        date = date + "th";
-    }
-
-
-    dateDisplay.textContent = `${matchDay(day)}, ${matchMonth(month)} ${date} ${year} `;
-
+  const clockDisplay = document.getElementById('timeDisplay');
+  clockDisplay.textContent = `${formatHour(
+    hours
+  )}:${minutes}:${seconds} ${amPm}`;
 }
 
-function matchMonth(month) {
-
-    const monthNames = [
-        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return monthNames[month];
+function renderLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
 }
 
-function matchDay(day) {
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
 
-    const dayNames = [
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return dayNames[day];
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
 }
 
+function renderDate() {
+  const dateObject = new Date();
+  let day = dayNames[dateObject.getDay()];
+  let month = monthNames[dateObject.getMonth()];
+  let date = dateObject.getDate();
+  let year = dateObject.getFullYear();
 
+  const dateDisplay = document.getElementById('dateDisplay');
+  dateDisplay.textContent = `${day}, ${month} ${appendDateSuffix(
+    date
+  )} ${year} `;
+}
 
-setInterval(digitalClock, 100);
-digitalClock();
+function appendDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
 
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
+const dayNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
+setInterval(() => {
+  renderClock();
+  renderDate();
+}, 1000);
+
+renderClock();
+renderDate();
